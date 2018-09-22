d255d4965236737661a125c9c15c97e94fa8315d
mairasala <mairasala@gmail.com>
2018-09-22 16:50:43 +0200

    lost changes

--- src/views/PostMessage.js
+++ src/views/PostMessage.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
 const styles = StyleSheet.create({
+  MessageContainer: {
+    paddingTop: 8,
+    paddingBottom: 8,
+    backgroundColor: '#dedede',
+    minHeight: 200,
+    maxHeight: 350,
+    alignItems: 'stretch',
+    justifyContent: 'center'
+  },
   PostMessage:{
     fontSize: 24,
-    paddingTop: 8,
-    paddingBottom: 8
+    backgroundColor: '#ffaaaa',
+  },
+  Photo:{
+    width: 100,
+    height: 100
   }
 });
 
 const PostMessage = ({post}) => {
+  console.log(post.photos && post.photos.length && post.photos[0]);
   return (
-    <Text style={styles.PostMessage}>{post.message}</Text>
+    <View style={styles.MessageContainer}>
+      <Text adjustsFontSizeToFit allowFontScaling minimumFontScale={0.1} style={styles.PostMessage}>{post.message}</Text>
+      {post.photos && post.photos.length > 0 &&
+        <Image style={styles.Photo} source={{uri:post.photos[0]}} />
+      }
+    </View>
   )
 }
 
